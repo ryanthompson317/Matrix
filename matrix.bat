79c9a2bda86e2426608dc4fd3405d98f2e9afbed
ryanthompson317 <ryanthompson317@u.boisestate.edu>
2019-09-19 21:53:32 -0600

    commit to the matrix

---
+++ matrix.bat
@@ -0,0 +1,9 @@
+REM To run in linux terminal:
+REM    $ wine cmd
+REM    $ start matrix.bat
+
+@echo off
+color 02
+:start
+echo %random% %random% %random% %random% %random% %random% %random% %random%
+goto start
